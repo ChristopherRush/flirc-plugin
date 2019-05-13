b391350bac6080df45208c084622664a0b59beaf
ChristopherRush <chris.uclan@gmail.com>
2019-05-13 13:13:00 +0100

    Create flirc-justboom.py
    Test

---
+++ flirc-justboom.py
@@ -0,0 +1,11 @@
+import keyboard  # using module keyboard
+import os
+while True:  # making a loop
+    try:  # used try so that if user pressed other than the given key error will not be shown
+        if keyboard.is_pressed('q'):  # if key 'q' is pressed
+            os.system("curl localhost:3000/api/v1/commands/?cmd=toggle")
+            break  # finishing the loop
+        else:
+            pass
+    except:
+        break  # if user pressed a key other than the given key the loop will break
